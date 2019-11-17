db3b194ce0d451f348e3e0faa10d861c48b6b2cf
Aliona Avilava <alionaavilava@alionas-mbp.home>
2019-11-17 18:50:50 +0000

    impliment date time getters

--- DateTime.js
+++ DateTime.js
@@ -1,7 +1,69 @@
 "use strict";
 var DateTime = (function() {
+    var monthNames = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ],
+      dayNames = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+      ];
+
     function createDateTime(date) {
         return {
+            get year() {
+                return date.getFullYear();
+            },
+            get monthName() {
+                return monthNames[date.getMonth()];
+            },
+            get month() {
+                return date.getMonth() + 1;
+            },
+            get day() {
+                return dayNames[date.getUTCDay()];
+            },
+            get date() {
+                return date.getUTCDate();
+            },
+            get ordinalDate() {
+                var n = this.date;
+                var suffix = "th";
+                if (n < 4 || n > 20) {
+                    suffix = ["st", "nd", "rd"][n % 10 - 1] || suffix;
+                }
+                return n + suffix;
+            },
+            get hours() {
+                return date.getUTCHours();
+            },
+            get hours12() {
+                return this.hours % 12 || 12;
+            },
+            get minutes() {
+                return date.getUTCMinutes();
+            },
+            get seconds() {
+                return date.getUTCSeconds();
+            },
+            get ampm() {
+                return this.hours < 12 ? "am" : "pm";
+            },
             get offset() {
                 return date.getTime();
             }
